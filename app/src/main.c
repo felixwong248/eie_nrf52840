65929cd3e5fc28bd18846705d15d6ee0416f78d4
Felix Wong <felixwong248@gmail.com>
2025-12-08 12:37:15 -0700

    added sleep param

--- app/src/main.c
+++ app/src/main.c
@@ -8,7 +8,7 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#define SW0_NODE DT_ATLAS(sw0)
+#define SW0_NODE DT_ALIAS(sw0)
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
 int main(void) {
@@ -28,7 +28,7 @@ int main(void) {
     if (0 < ret){
       printk("Pressed!\n");
     }
-    k_msleep(SLEEP_TIME_MS);
+    k_msleep(1000);
   }
 	return 0;
 }
