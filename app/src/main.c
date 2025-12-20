755804f1965a6186f720a237998f5f1c95139712
Felix Wong <felixwong248@gmail.com>
2025-12-19 20:36:58 -0700

    fixed variable count

--- app/src/main.c
+++ app/src/main.c
@@ -5,16 +5,30 @@
 #include <inttypes.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 
+#include "BTN.h"
+#include "LED.h"
+#include "state_machine.h"
 
 #define SLEEP_MS 1
 
 int main(void) {
-
-
-  while(1) {
-
-	return 0;
-  }
+    if (0 > BTN_init()) {
+    return 0;
+    }
+    if (0 > LED_init()) {
+    return 0;
+    }
+    state_machine_init();
+    while(1) {
+    int ret = state_machine_run();
+    if (0 > ret){
+    return 0;
+    }
+    k_msleep(SLEEP_MS);
+    }
+    return 0;
 }
