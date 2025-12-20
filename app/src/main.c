c27232e425b01310a6ce344064066fe98ef827fb
Felix Wong <felixwong248@gmail.com>
2025-12-19 20:29:35 -0700

    added states and their functions

--- app/src/main.c
+++ app/src/main.c
@@ -7,22 +7,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-#include "BTN.h"
-#include "LED.h"
 
 #define SLEEP_MS 1
 
 int main(void) {
 
-  if (0 > BTN_init()) {
-    return 0;
-  }
-  if (0 > LED_init()) {
-    return 0;
-  }
 
   while(1) {
-    k_msleep(SLEEP_MS);
-  }
+
 	return 0;
+  }
 }
